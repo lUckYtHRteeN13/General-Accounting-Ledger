6c783b20c6d9542a43b2c2c7c8b4cca5d1b7e7f1
Nethan Quinn G. Jael (Lucky13) <76927645+lUckYtHRteeN13@users.noreply.github.com>
2025-03-17 09:53:09 +0800

    Add files via upload

---
+++ Ledger Manager/src/Utils/DatabaseManager.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+/**
+ *
+ * @author LuckyThirteen
+ */
+public class DatabaseManager {
+ 
+    public DatabaseManager() {
+    
+    }
+    
+    public void connect() {
+    
+    }
+}
